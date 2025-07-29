bcc229f5463f99599006458cbe2deaf9083e51f7
Bruno Romero de Azevedo <brunordea@gmail.com>
2025-07-29 19:02:14 +0200

    Toggle debug lines on F1

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use bevy::{color::palettes::css::*, prelude::*};
 use bevy_prototype_lyon::prelude::*;
 
 #[derive(Resource)]
+struct DebugLinesEnabled(bool);
+
+#[derive(Resource)]
 struct BlockSize(f32);
 
+// FIXME: this can probably be removed
 #[derive(Resource)]
 struct InitialGridPosition(u32, u32);
 
-#[derive(Debug, Component)]
+#[derive(Debug, Component, Copy, Clone)]
 struct GridPosition {
     col: u32,
     row: u32,
@@ -149,22 +153,70 @@ fn render_grid(
         transform: Transform::from_xyz(position.x, position.y, 0f32),
         ..default()
     };
-
     commands.spawn((outline, Stroke::new(stroke.color, 2.0)));
 }
 
-fn spawn_t(mut commands: Commands, initial_grid_position: Res<InitialGridPosition>) {
+fn toggle_debug_lines_enabled(
+    mut enabled: ResMut<DebugLinesEnabled>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard.just_pressed(KeyCode::F1) {
+        enabled.0 = !enabled.0;
+    }
+}
+
+fn grid_debug_lines(
+    enabled: Res<DebugLinesEnabled>,
+    block_size: Res<BlockSize>,
+    grid: Single<(&Grid, &Position)>,
+    mut gizmos: Gizmos,
+) {
+    if enabled.0 {
+        let (grid, position) = *grid;
+        let block_size = block_size.0;
+        let grid_width = grid.cols as f32 * block_size;
+        let grid_height = grid.rows as f32 * block_size;
+        let y = -grid_height;
+        for col in 1..grid.cols {
+            let x = block_size * col as f32;
+            gizmos.line_2d(
+                Vec2::new(position.x + x, position.y),
+                Vec2::new(position.x + x, position.y + y),
+                BLUE_VIOLET,
+            );
+        }
+        let x = grid_width;
+        for row in 1..grid.rows {
+            let y = block_size * row as f32 * -1.0;
+            gizmos.line_2d(
+                Vec2::new(position.x, position.y + y),
+                Vec2::new(position.x + x, position.y + y),
+                BLUE_VIOLET,
+            );
+        }
+    }
+}
+
+fn spawn_t(mut commands: Commands, grid_position: GridPosition) {
+    info!("Spawning T at {:?}", grid_position);
     let (shape, rotations) = Shape::t();
     commands.spawn((
         shape,
         rotations,
+        grid_position,
+        Fill::color(RED),
+        ActivePiece,
+    ));
+}
+
+fn initial_spawn_t(commands: Commands, initial_grid_position: Res<InitialGridPosition>) {
+    spawn_t(
+        commands,
         GridPosition {
             col: initial_grid_position.0,
             row: initial_grid_position.1,
         },
-        Fill::color(RED),
-        ActivePiece,
-    ));
+    );
 }
 
 fn block_transform(
@@ -191,7 +243,7 @@ fn render_active_piece(
         for block_offset in rotations.cur_offsets().offset_positions.as_ref() {
             let shape = shapes::Rectangle {
                 extents: Vec2::new(block_size.0, block_size.0),
-                origin: RectangleOrigin::Center,
+                origin: RectangleOrigin::TopLeft,
                 ..default()
             };
 
@@ -209,7 +261,7 @@ fn render_active_piece(
             commands.spawn((
                 outline,
                 Fill::color(color.color),
-                Stroke::new(BLACK, 2.0),
+                Stroke::new(BLACK, 1.0),
                 BlockIdx(block_offset.2),
                 ActivePiece,
             ));
@@ -227,7 +279,7 @@ fn rotate_piece(
     if keyboard.just_pressed(KeyCode::Space) {
         single_piece.0.rotate();
         let cur_offsets = single_piece.0.cur_offsets();
-        let grid_position = &single_piece.1;
+        let grid_position = single_piece.1;
 
         for (mut transform, idx) in &mut query_block {
             let block_offset = cur_offsets.offset_positions[idx.0];
@@ -241,19 +293,84 @@ fn rotate_piece(
     }
 }
 
+#[derive(Resource)]
+struct GravityTimer(Timer);
+
+#[derive(Resource)]
+struct GravitySpeed(f32);
+
+fn apply_gravity(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut timer: ResMut<GravityTimer>,
+    mut single_piece: Single<
+        (Entity, &StandardPieceRotations, &mut GridPosition),
+        With<ActivePiece>,
+    >,
+    mut query_block: Query<&mut Transform, With<ActivePiece>>,
+    block_size: Res<BlockSize>,
+    grid: Single<&Grid>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        single_piece.2.row += 1;
+        for mut transform in &mut query_block {
+            transform.translation.y -= block_size.0;
+        }
+        let mut lowest_row_offset = None;
+        for offset in single_piece.1.cur_offsets().offset_positions {
+            let block_row_offset = offset.2;
+            if lowest_row_offset.is_none() {
+                lowest_row_offset = Some(block_row_offset);
+            } else if let Some(lowest) = lowest_row_offset {
+                if block_row_offset < lowest {
+                    lowest_row_offset = Some(block_row_offset);
+                }
+            }
+        }
+        if let Some(lowest_row_offset) = lowest_row_offset {
+            // check if the bottom block is touching the bottom of the board
+            let piece_row = single_piece.2.row + lowest_row_offset as u32;
+            dbg!(&piece_row);
+            dbg!(&grid.rows);
+            if piece_row >= grid.rows {
+                warn!("Removed active piece!");
+                commands.entity(single_piece.0).remove::<ActivePiece>();
+            }
+        }
+    }
+}
+
+fn increase_gravity(
+    mut timer: ResMut<GravityTimer>,
+    mut speed: ResMut<GravitySpeed>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard.just_pressed(KeyCode::ArrowDown) {
+        *speed = GravitySpeed(speed.0 + 0.5);
+        info!("New gravity speed: {:?}", speed.0);
+        // FIXME: 2.0 should be some const variable, maybe another Res?
+        *timer = GravityTimer(Timer::from_seconds(2.0 / speed.0, TimerMode::Repeating));
+    }
+}
+
+// TODO: add a "GameState" resource or something so we can have "Paused" and we pause gravity and everything
 fn main() {
     App::new()
         .insert_resource(BlockSize(30f32))
+        .insert_resource(DebugLinesEnabled(false))
         .insert_resource(InitialGridPosition(5, 0))
+        .insert_resource(GravityTimer(Timer::from_seconds(0.5, TimerMode::Repeating)))
+        .insert_resource(GravitySpeed(1.0))
         .add_plugins((DefaultPlugins, ShapePlugin))
         .add_systems(
             Startup,
             (
                 setup,
                 (spawn_grid, render_grid).chain(),
-                (spawn_t, render_active_piece).chain(),
+                (initial_spawn_t, render_active_piece).chain(),
             ),
         )
-        .add_systems(Update, rotate_piece)
+        .add_systems(Update, (increase_gravity, rotate_piece))
+        .add_systems(Update, (toggle_debug_lines_enabled, grid_debug_lines))
         .run();
 }
